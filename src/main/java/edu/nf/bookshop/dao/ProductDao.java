2dff54fd564b93fe5e2333f586fe0288c6b0a705
hjl <2010332047@qq.com>
2020-11-25 16:03:22 +0800

    Add dao

---
+++ src/main/java/edu/nf/bookshop/dao/ProductDao.java
@@ -0,0 +1,13 @@
+package edu.nf.bookshop.dao;
+
+import edu.nf.bookshop.entity.Product;
+
+/**
+ * @Author: 傻逼黎汉清
+ * @Date: 2020/11/25 16:00
+ */
+public class ProductDao {
+    public void save(Product product){
+
+    }
+}
